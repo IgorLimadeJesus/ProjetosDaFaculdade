55d06a730918d1a829eb2fbe863869e0c5f7c19e
IgorLima <163208539+IgorLimadeJesus@users.noreply.github.com>
2024-03-16 22:41:59 -0300

    Add files via upload

--- CalculodeMedia.c
+++ CalculodeMedia.c
@@ -2,25 +2,32 @@
 
 int main(){
 
+//Variaveis
 float nota1, nota2, media;
+int Freq;
 
-printf("Digite uma nota: ");
+printf("Digite uma nota: "); //recebe o valor e armazena na variavel nota1
 scanf("%f",&nota1);
 
-printf("Digite outra nota: ");
+printf("Digite outra nota: "); //recebe o valor e armazena na variavel nota2
 scanf("%f",&nota2);
 
-media = (nota1+nota2)/2;
+printf("Digite sua frequencia");
+scanf("%i",&Freq);
 
-if (media >= 5.5)
+media = (nota1+nota2)/2; //faz o calculo da media
+
+if (media >= 5.5 && Freq >= 75) //faz a verificação se está aprovado ou não
 {
-    printf("\nAprovado");
+    printf("\nAprovado"); //mostra o resultado
     printf("\nSua media: %f",media);
+    printf("Sua Frequencia e: %i", Freq);
 }
 else
 {
     printf("\nReprovado");
     printf("\nSua media e: %f",media);
+    printf("Sua Frequencia e: %i",Freq);
 }
 
 
